ab6df4f65eebf2f94770e1772e234ccce414e6b4
RomanKosmyna <roma.kosmyna@gmail.com>
2023-02-28 16:35:07 +0200

    Homework 1

---
+++ app.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+const path = require('path');
+
+// fs.mkdir(path.join('folder'), (err) => {
+//     if (err) throw new Error();
+//     for (let i =1; i < 6; i++) {
+//         fs.writeFile(path.join(__dirname, 'folder', `${i}.txt`), `Hello file number ${i}`, (err) => {
+//             if (err) throw new Error(err.message);
+//         })
+//     }
+//     for (let q = 1; q < 6; q++) {
+//         fs.mkdir(path.join(__dirname, 'folder', `${q}`), (err) => {
+//             if (err) throw new Error();
+//         })
+//     }
+// });
+
+// fs.readdir(path.join('folder'), {withFileTypes: true}, (err, data) => {
+//     console.log(data);
+//     data.forEach(file => {
+//         file.isFile() ? console.log(`File: ${file.name}`): console.log(`Folder: ${file.name}`);
+//     })
+// });
+
+fs.readdir(path.join('folder'), {withFileTypes: true}, (err, data) => {
+    if (err) throw new Error();
+    data.map(file => {
+        file.isFile() ? console.log(`File: ${file.name}`): console.log(`Folder: ${file.name}`)
+    })
+})
